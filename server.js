91c2bb8502e4ed49be8446ca0ab7862ce8d08a43
Fangzhou Li <FLI84@seneca.ca>
2021-07-04 22:08:44 -0400

    port update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const nodemailer = require('nodemailer');
 const app = express()
-const port = process.env.PORT | 80
+const port = process.env.port || 80
 
 app.use(express.static('public')); 
 
